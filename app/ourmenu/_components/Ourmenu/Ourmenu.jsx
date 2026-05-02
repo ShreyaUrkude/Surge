4e45eb99437ee091266f0f6b509dea8bffb9c4d3
Tamizh1202 <tamilselvan9359@gmail.com>
2026-05-02 10:35:18 +0530

    2nd morning

--- app/ourmenu/_components/Ourmenu/Ourmenu.jsx
+++ app/ourmenu/_components/Ourmenu/Ourmenu.jsx
@@ -1,10 +1,8 @@
 import styles from "./Ourmenu.module.css";
 
 export default function OurMenu(){
-  
   return (
          <section className={styles.bannerWrapper}>
-     
     </section>
   );
 }
